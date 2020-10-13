c8478fe2ead38066a5c9c3cef0f4f68f9fd09587
sonderius <lubomir.zavodsky@outlook.sk>
2020-10-13 20:41:57 +0200

    presun

---
+++ proj/scaner.c
@@ -0,0 +1,196 @@
+/*
+* autor:  Lubomír Závodský <xzavod14>									*
+* rok vytvoření: 2020		                            	*
+* projekt ifj 2020/2021                               *
+*/
+
+// lexikalni analyzator
+//library
+#include <stdio.h>
+#include <ctype.h>
+#include "scaner.h"
+char c; // variable for load chars from input
+
+int i = 1; // pocitadlo na zastavenie
+//inicializace tokenu
+Token *T_init()
+{
+	struct Token *token = malloc(sizeof(Token));
+
+
+	return token;
+}
+
+void setSourceFile(FILE *f)
+{
+	source = f;
+
+	printf("loading file\n" );
+
+	if (source == NULL)
+	{
+		printf("Null file\n");
+		return;
+	}
+} // funkce na citanie suboru
+
+
+
+// hlavni funkce lexikalniho analyzatoru //prerobit main na funkciu co vrati tokeny
+Token *getNextToken()
+{
+	printf("1.analizing start\n");
+	printf("start load file\n");
+
+	setSourceFile(stdin);// for testing must be delete
+	static T_state state = START; // stav v ktorom sa bude začinat
+
+	Token *token = T_init();
+		printf("token init\n");
+	while (i)
+	{
+
+		c = getc(source);
+		printf("%c\n",c);
+		switch (state)
+		{
+			case START:
+
+				if (c == '\n')
+				{
+					state = EOLINE;
+					printf("EOLINE\n");
+				}
+
+				else if (c == '_' || isalpha(c))
+				{
+					state = ID;
+
+				}
+								//ČISLO
+				else if (isdigit(c))
+					state = INT;
+
+									//operatory
+				else if (c == '-')
+					state = MINUS;
+				else if (c == '+')
+					state = PLUS;
+				else if (c == '*')
+					state = MULTIPLE;
+				//	else if (c == '/')
+					//	state = TESTDIVIDE;
+					// komentar dorobit / MOzE byt delenie
+
+				//logic operators
+				else if (c == '!')
+					state = NOT;
+				else if (c == '&')
+					state = AND;
+				else if (c == '|')
+					state = OR;
+				//others char
+
+				else if (c == '(')
+					state = LEFTBR;
+				else if (c == ')')
+					state = RIGHTBR;
+				else if (c == ':')
+					state = DOUBLEDOT;
+				else if (c == ',')
+					state = COMMA;
+				else if (c == '=')
+					state = ASSIGN;
+				// less MORE
+				else if (c == '<')
+					state = LESS;
+				else if (c == '>')
+					state = MORE;
+				// space
+				else if (isspace(c))
+					state = SPACE;
+					//end of file
+					else if (c == EOF)
+					{
+						printf("EOF\n");
+
+						//priradit typ tokenu
+						token->type = T_END_OF_FILE;
+
+						state = END;
+
+						break;
+					}
+					else
+					{
+						//poslat NULL
+
+						return NULL;
+					}
+					break;
+//dalsie stavy
+case EOLINE:
+
+		printf("STAV  EOLINE\n" );
+		ungetc(c, source);
+		token->type = T_EOL;
+		state = START;
+		return token;
+
+
+
+
+
+		}//end of switch
+	}//end of while
+
+}
+int main() {
+	while (1) {
+
+	print_token(getNextToken());
+
+	}
+
+	return 0;
+}
+//funkce na vizualnu kontrolu tokenov
+void print_token(Token *token)
+{
+	static long counter = 1;
+
+	printf("\n--------------------\n");
+	printf("%ld. token\n", counter++);
+	printf("of type: ");
+
+	switch (token->type)
+	{
+	case T_UNKNOWN:
+		printf("UNKNOWN");
+		break;
+
+
+	case T_EOL:
+		printf("EOL");
+		break;
+
+	}
+
+	printf("\n");
+// tiskne na konci "\n"
+	printf("--------------------\n");
+}
+
+//testovanie
+/*int test()
+{
+	Token *token;
+	token = getNextToken();
+	while (token->type != T_END_OF_FILE)
+	{
+		token = getNextToken();
+		print_token(token);
+	}
+
+	return 1;
+}*/
---
+++ proj/scaner.h
@@ -0,0 +1,106 @@
+/*
+* autor:  Lubomír Závodský <xzavod14>	                *
+* rok vytvoření: 2020		                            	*
+* projekt ifj 2020/2021                             *
+*/
+//hlavicka pro lexikalni analyzator
+
+#include "err.h"
+// vytvorit knihovnu na kruhovy seznam
+
+//jednotlive typy
+typedef enum
+{
+
+  T_UNKNOWN, //Výchozí typ
+  T_ID,      //identifikator
+             //klicova slova
+  T_ELSE,    // Keyword - ELSE
+  T_INT,
+  T_DOUBLE,
+  T_COMENT, // //
+  //operatory
+  T_PLUS,   //+
+  T_MINUS,  //-
+  T_MUL,    // *
+  T_DIV,    // /
+  T_INTDIV, // //
+  T_STRING, // retazec
+
+  // logic operators
+  T_NOT,
+  T_AND,
+  T_OR,
+  T_EQ_COMP, //
+  // others char
+  T_LEFTBR,   // (
+  T_RIGHTBR,  // )
+  T_COMMA,    //.
+  T_ASSIGN,   // =
+  T_NOTEQUAL, // !=
+  T_SPACE,    // medzera
+  T_DOUBLEDOT, //:
+  //porovnavanie
+  T_LESS, //<
+  T_LESS_EQ,
+  T_MORE, //>
+  T_MORE_EQ,
+  //odsadenie
+  T_INDENT,
+  T_DEDENT,
+  //
+  T_EOL, //koniec riadku
+  T_END_OF_FILE // koniec vstupneho suboru
+
+} tType;
+//stavy automatu
+typedef enum
+{
+  BEGLINE,  //stav začiatok riadku kontrola odsadenia
+  EOLINE,   // END OF line
+  START,    // počiatočny stav ak nebolo odsadenie
+  ID,       //identifikator
+  KEYW,     // klučove slovo
+  INT,      // číslo
+  DOUBLE,   // desatine čislo
+  STRING,   // stav kontrolujuci retazec
+  HEX,      //
+  HEX_CON,  //
+  ESC,      //escape sekvencia
+  MINUS,    //operatory
+  PLUS,     // +
+  MULTIPLE, // -
+  DIVIDE,   // delenie
+  SPACE,    //space
+  NOT,
+  AND,
+  OR,
+  LEFTBR,    //(
+  RIGHTBR,   // )
+  COMMA,     //,
+  ASSIGN,    //=
+  LESS,      //<
+  MORE,      //>
+  DOUBLEDOT, //:
+  END,        // koncový stav
+} T_state;
+/*typedef
+
+    dynstr_t *
+        tData;
+*/
+// definice structury tokenu
+typedef struct Token
+{
+  tType type;
+//  tData data;
+
+} Token;
+
+Token *T_init();
+
+FILE *source;                // promenna pro ulozeni vstupniho souboru
+//funkce scaneru
+void setSourceFile(FILE *f); // funkce pre ulo6enie suboru na nacitanie
+int main();
+Token *getNextToken();  // hlavna funkcia
