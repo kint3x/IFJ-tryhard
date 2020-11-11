60b73087abd5a7728e934e0ff66e4cead5985181
Matejkopro <58399747+Matejkopro@users.noreply.github.com>
2020-11-11 21:15:56 +0100

    Update expr.h
    kozmeticke upravy

--- proj/expr.h
+++ proj/expr.h
@@ -1,29 +1,35 @@
 #ifndef _EXPR_H
 #define _EXPR_H
-
-
+#include <stdbool.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 typedef enum {
-	PLUS,			// +
-	MINUS,			// -
-	MUL,			// *
-	DIV,			// /
-	LEFT_BR,		// (
-	RIGHT_BR,		// )
-	ID,				// i
-	LESS,			// <
-	MORE,			// >
-	LESS_EQ,		// <=
-	MORE_EQ,		// >=
-	EQUAL,			// ==
-	NOT_EQUAL,		// !=
-	DOLLAR,			// $
-	INT,			// int
-	STRING,			// str
-	FLOAT,			// float
-	NON_TERMINAL,	// E
-	ERR				// pre T_ERR
+	L_PLUS,			// +
+	L_MINUS,		// -
+	L_MUL,			// *
+	L_DIV,			// /
+	L_LEFT_BR,		// (
+	L_RIGHT_BR,		// )
+	L_ID,			// i
+	L_LESS,			// <
+	L_MORE,			// >
+	L_LESS_EQ,		// <=
+	L_MORE_EQ,		// >=
+	L_EQUAL,		// ==
+	L_NOT_EQUAL,	// != 
+	L_DOLLAR,		// $
+	L_INT,			// int
+	L_STRING,		// str
+	L_FLOAT,		// float
+	L_NON_TERMINAL,	// E
+	L_ERR			// pre T_ERR alebo T_UNKNOWN
 
 } expr_lexem;
+
+expr_lexem token_convert();
+int items_above_handle();
+int reduce_stack(bool *rel_flag);
+int expression();
 
 #endif
