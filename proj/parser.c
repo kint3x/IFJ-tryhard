7183b167ecea270287b68241dc5331a05359d1b6
Matejkopro <58399747+Matejkopro@users.noreply.github.com>
2020-11-11 21:22:20 +0100

    Update parser.c
    dorobene funkcie s expr, su zle ale, cely parser treba prejst

--- proj/parser.c
+++ proj/parser.c
@@ -2,58 +2,57 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "parser.h"
-
-#define VALUE_CHECK() if (ret_value != ERR_RIGHT) return ret_value;
-#define GET_TOKEN() p_getnexttoken(); if ((token.type == T_UNKNOWN) || (token.type == T_ERR)) return ERR_LEXSCAN; 
+#include "scaner.h"
+#include "expr.h"
+#include "stack.h"
+#include "err.h"
+#include "dynstring.h"
 
 Token token; // GLOBALNA PREMENNA S AKTUALNYM TOKENOM
 Token tokenp; // pomocny token
 
-void peek_nexttoken(){
-		Token *t = getNextToken();   // NACITAME NOVY TOKEN
-		print_token(t);
-		tokenp.type=t->type;
-		tokenp.data=t->data;
-		free(t);					// UVOLNIME STRUKTURU 
-		t=NULL;
+void peek_nexttoken() {
+	Token *t = getNextToken();   // NACITAME NOVY TOKEN
+	print_token(t);
+	tokenp.type = t->type;
+	tokenp.data = t->data;
+	free(t);					// UVOLNIME STRUKTURU 
+	t = NULL;
 }
 
-void p_getnexttoken(){
-	if(token.type==T_END_OF_FILE){  //ROBILO TO MEMORY LEAK AK TU TATO PODMIENKA NEBOLA, Z NEJAKOHO DOVODU
+void p_getnexttoken() {
+	if (token.type == T_END_OF_FILE) {  //ROBILO TO MEMORY LEAK AK TU TATO PODMIENKA NEBOLA, Z NEJAKOHO DOVODU
 		nstring_free(token.data);       // SI TO PYTALO DALSI TOKEN PO EOF
 		return;
 	}
 
-	if(tokenp.type!=-1){
+	if (tokenp.type != -1) {
 		nstring_free(token.data);
-		token.type= tokenp.type;
-		token.data= tokenp.data;
+		token.type = tokenp.type;
+		token.data = tokenp.data;
 		tokenp.type = -1;
-		tokenp.data= NULL;
+		tokenp.data = NULL;
 	}
-	else{
+	else {
 		nstring_free(token.data);  // VYMAZE SA POVODY NSTRING
-		token.data=NULL;
+		token.data = NULL;
 
 		Token *t = getNextToken();   // NACITAME NOVY TOKEN
 		print_token(t);
-		token.type=t->type;
-		token.data=t->data;
+		token.type = t->type;
+		token.data = t->data;
 		free(t);					// UVOLNIME STRUKTURU 
-		t=NULL;
+		t = NULL;
 	}
 }
 
-int expr_or_cond(){
-	return 0;//TATO FUNKCIA BUDE HANDLOVAT CI JE TO EXPRESSION ALEBO CONDITION
-}
 
 int p_prog() {
 	printf("SET token.data to null\n");
-	token.type=T_UNKNOWN; // Inicializacia 
-	token.data=NULL; // Nastavi dynstring na NULL
-	tokenp.data=NULL;
-	tokenp.type=-1;
+	token.type = T_UNKNOWN; // Inicializacia 
+	token.data = NULL; // Nastavi dynstring na NULL
+	tokenp.data = NULL;
+	tokenp.type = -1;
 
 	int ret_value = ERR_SYNAN;
 	GET_TOKEN();
@@ -72,8 +71,8 @@ int p_prog() {
 						ret_value = p_funclist();
 						VALUE_CHECK();
 					}
-					else if(token.type== T_END_OF_FILE){
-						ret_value=ERR_RIGHT;
+					else if (token.type == T_END_OF_FILE) {
+						ret_value = ERR_RIGHT;
 					}
 				}
 			}
@@ -88,22 +87,22 @@ int p_funclist() {
 	int ret_value = ERR_SYNAN;
 
 	switch (token.type) {
-		case T_WFUNC:
-			ret_value = p_func();
-			VALUE_CHECK();
-			ret_value = p_opteol();
-			VALUE_CHECK();
-			ret_value = p_funclist();
-			VALUE_CHECK();
-			break;
+	case T_WFUNC:
+		ret_value = p_func();
+		VALUE_CHECK();
+		ret_value = p_opteol();
+		VALUE_CHECK();
+		ret_value = p_funclist();
+		VALUE_CHECK();
+		break;
 
-		case T_END_OF_FILE:
-			GET_TOKEN();
+	case T_END_OF_FILE:
+		GET_TOKEN();
 
-			ret_value = ERR_RIGHT;
-			break;
-		default:
-			break;
+		ret_value = ERR_RIGHT;
+		break;
+	default:
+		break;
 	}
 
 	return ret_value;
@@ -115,25 +114,25 @@ int p_func() {
 
 	if (token.type == T_WFUNC) {
 		GET_TOKEN();
-		
+
 		if (token.type == T_ID) {
 			GET_TOKEN();
-			
+
 			if (token.type == T_LEFTBR) {
 				GET_TOKEN();
-				
+
 				ret_value = p_paramlist();
 				VALUE_CHECK();
 				ret_value = p_datatypelist();
 				VALUE_CHECK();
 				ret_value = p_statlist();
 				VALUE_CHECK();
-				
+
 				if (token.type == T_EOL) {
 					GET_TOKEN();
 				}
-				else if(token.type==T_END_OF_FILE){
-					ret_value=ERR_RIGHT;
+				else if (token.type == T_END_OF_FILE) {
+					ret_value = ERR_RIGHT;
 				}
 				else ret_value = ERR_SYNAN;
 			}
@@ -148,22 +147,22 @@ int p_paramlist() {
 	int ret_value = ERR_SYNAN;
 
 	switch (token.type) {
-		case T_ID:
-			GET_TOKEN();
+	case T_ID:
+		GET_TOKEN();
 
-			ret_value = p_datatype();
-			VALUE_CHECK();
-			ret_value = p_paramnext();
-			VALUE_CHECK();
-			break;
+		ret_value = p_datatype();
+		VALUE_CHECK();
+		ret_value = p_paramnext();
+		VALUE_CHECK();
+		break;
 
-		case T_RIGHTBR:
-			GET_TOKEN();
+	case T_RIGHTBR:
+		GET_TOKEN();
 
-			ret_value = ERR_RIGHT;
-			break;
-		default:
-			break;
+		ret_value = ERR_RIGHT;
+		break;
+	default:
+		break;
 	}
 
 	return ret_value;
@@ -194,7 +193,7 @@ int p_paramnext() {
 		ret_value = ERR_RIGHT;
 		break;
 	default:
-			break;
+		break;
 	}
 
 	return ret_value;
@@ -226,7 +225,7 @@ int p_datatypelist() {
 		VALUE_CHECK();
 		break;
 	default:
-			break;
+		break;
 	}
 
 	return ret_value;
@@ -263,7 +262,7 @@ int p_datatypenext() {
 		VALUE_CHECK();
 		break;
 	default:
-			break;
+		break;
 	}
 
 	return ret_value;
@@ -274,25 +273,25 @@ int p_datatype() {
 	int ret_value = ERR_SYNAN;
 
 	switch (token.type) {
-		case T_WINT:
-			GET_TOKEN();
+	case T_WINT:
+		GET_TOKEN();
 
-			ret_value = ERR_RIGHT;
-			break;
+		ret_value = ERR_RIGHT;
+		break;
 
-		case T_WFLOAT64:
-			GET_TOKEN();
+	case T_WFLOAT64:
+		GET_TOKEN();
 
-			ret_value = ERR_RIGHT;
-			break;
+		ret_value = ERR_RIGHT;
+		break;
 
-		case T_WSTRING:
-			GET_TOKEN();
+	case T_WSTRING:
+		GET_TOKEN();
 
-			ret_value = ERR_RIGHT;
-			break;
-		default:
-			break;
+		ret_value = ERR_RIGHT;
+		break;
+	default:
+		break;
 	}
 
 	return ret_value;
@@ -303,121 +302,148 @@ int p_statlist() {
 	int ret_value = ERR_SYNAN;
 
 	switch (token.type) {
-		case T_WIF:
-		case T_WFOR:
-		case T_WRETURN:
-		case T_ID:
-			ret_value = p_stat();
-			VALUE_CHECK();
+	case T_WIF:
+	case T_WFOR:
+	case T_WRETURN:
+	case T_ID:
+		ret_value = p_stat();
+		VALUE_CHECK();
 
-			if (token.type == T_EOL) {
-				GET_TOKEN();
-			}
-			else ret_value = ERR_SYNAN;
-
-			ret_value = p_opteol();
-			VALUE_CHECK();
-			ret_value = p_statlist();
-			VALUE_CHECK();
-			break;
-
-		case T_RIGHTBRACET:
+		if (token.type == T_EOL) {
 			GET_TOKEN();
+		}
+		else ret_value = ERR_SYNAN;
 
-			ret_value = ERR_RIGHT;
-			break;
-		default:
-			break;
+		ret_value = p_opteol();
+		VALUE_CHECK();
+		ret_value = p_statlist();
+		VALUE_CHECK();
+		break;
+
+	case T_RIGHTBRACET:
+		GET_TOKEN();
+
+		ret_value = ERR_RIGHT;
+		break;
+	default:
+		break;
 	}
 
 	return ret_value;
 }
 
 int p_stat() {
-	// #TODO Kebabova implementacia zatial
-	int ret_value=ERR_SYNAN;
-		switch(token.type){
-			case T_WIF:
+
+	int ret_value = ERR_SYNAN;
+
+	switch (token.type) {
+	case T_WIF:
+		GET_TOKEN();
+		
+		ret_value = expression();
+		VALUE_CHECK();
+
+		if (token.type == T_LEFTBRACET) {
+			GET_TOKEN();
+
+			if (token.type == T_EOL) {
 				GET_TOKEN();
-				ret_value=expr_or_cond();
+
+				ret_value = p_opteol();
 				VALUE_CHECK();
-				if(token.type!=T_LEFTBRACET) return ERR_SYNAN;
-				GET_TOKEN();
-				if(token.type!=T_EOL) return ERR_SYNAN;
-				GET_TOKEN();
-				ret_value=p_opteol();
+				ret_value = p_statlist();
 				VALUE_CHECK();
-				ret_value=p_statlist();
-				VALUE_CHECK();
-				if(token.type!=T_WELSE) return ERR_SYNAN;
-				GET_TOKEN();
-				if(token.type!=T_LEFTBRACET) return ERR_SYNAN;
-				GET_TOKEN();
-				if(token.type!=T_EOL) return ERR_SYNAN;
-				GET_TOKEN();
-				ret_value=p_opteol();
-				VALUE_CHECK();
-				ret_value=p_statlist();
-				VALUE_CHECK();		
-				ret_value=ERR_RIGHT;		
-				break;
-			case T_WFOR:
-				GET_TOKEN();
-				ret_value=p_defstat();
-				VALUE_CHECK();
-				if(token.type!=T_SEMI) return ERR_SYNAN;
-				GET_TOKEN();
-				ret_value=expr_or_cond();
-				VALUE_CHECK();
-				if(token.type!=T_SEMI) return ERR_SYNAN;
-				GET_TOKEN();
-				ret_value=p_assignstat();
-				VALUE_CHECK();
-				if(token.type!=T_LEFTBRACET) return ERR_SYNAN;
-				GET_TOKEN();
-				if(token.type!=T_EOL) return ERR_SYNAN;
-				GET_TOKEN();
-				ret_value=p_statlist();
-				VALUE_CHECK();
-				ret_value=ERR_RIGHT;
-				break;
-			case T_WRETURN:
-				GET_TOKEN();
-				ret_value=p_expressionlist();
-				VALUE_CHECK();
-				ret_value=ERR_RIGHT;
-				break;
-			case T_ID:
-				GET_TOKEN();
-				ret_value=p_idstat();
-				VALUE_CHECK();
-				ret_value=ERR_RIGHT;
-			default:
-				break;
+
+				if (token.type == T_WELSE) {
+					GET_TOKEN();
+
+					if (token.type == T_LEFTBRACET) {
+						GET_TOKEN();
+
+						if (token.type == T_EOL) {
+							GET_TOKEN();
+
+							ret_value = p_opteol();
+							VALUE_CHECK();
+							ret_value = p_statlist();
+							VALUE_CHECK();
+						}
+					}
+				}
+			}
 		}
+		break;
+	case T_WFOR:
+		GET_TOKEN();
+
+		ret_value = p_defstat();
+		VALUE_CHECK();
+
+		if (token.type == T_SEMI) {
+			GET_TOKEN();
+
+			ret_value = expression();
+			VALUE_CHECK();
+
+			if (token.type == T_SEMI) {
+				GET_TOKEN();
+
+				ret_value = p_assignstat();
+				VALUE_CHECK();
+				
+				if (token.type == T_LEFTBRACET) {
+					GET_TOKEN();
+
+					if (token.type == T_EOL) {
+						GET_TOKEN();
+
+						ret_value = p_statlist();
+						VALUE_CHECK();
+					}
+				}
+			}
+		}
+		break;
+	case T_WRETURN:
+		GET_TOKEN();
+
+		ret_value = p_expressionlist();
+		VALUE_CHECK();
+		break;
+	case T_ID:
+		GET_TOKEN();
+
+		ret_value = p_idstat();
+		VALUE_CHECK();
+		break;
+	default:
+		break;
+	}
 	return ret_value;
 }
 
 int p_defstat() {
-	// #TODO Kebabova implementacia zatial
-	int ret_value=ERR_SYNAN;
-		switch(token.type){
-			case T_ID:
-				GET_TOKEN();
-				if(token.type!=T_DOUBLEDOT) return ERR_SYNAN;
-				GET_TOKEN();
-				if(token.type!=T_ASSIGN) return ERR_SYNAN;
-				GET_TOKEN();
-				ret_value=expr_or_cond();
-				VALUE_CHECK();
-				ret_value=ERR_RIGHT;	
-				break;
-			case T_SEMI:
-				ret_value=ERR_RIGHT;
-				break;
-			default:
-				break;
-		}
+	
+	int ret_value = ERR_SYNAN;
+
+	switch (token.type) {
+	case T_ID:
+		GET_TOKEN();
+		if (token.type != T_DOUBLEDOT) {     
+			GET_TOKEN();
+
+			ret_value = expression();
+			VALUE_CHECK();
+		} 
+		break;
+
+	case T_SEMI:
+		ret_value = ERR_RIGHT;
+		break;
+
+	default:
+		break;
+	}
 	return ret_value;
 }
 
@@ -426,18 +452,18 @@ int p_assignstat() {
 	int ret_value = ERR_SYNAN;
 
 	switch (token.type) {
-		case T_ID:
-			ret_value = p_idlist();
-			VALUE_CHECK();
-			ret_value = p_expressionlist();
-			VALUE_CHECK();
-			break;
+	case T_ID:
+		ret_value = p_idlist();
+		VALUE_CHECK();
+		ret_value = p_expressionlist();
+		VALUE_CHECK();
+		break;
 
-		case T_LEFTBRACET:
-			ret_value = ERR_RIGHT;
-			break;
-		default:
-			break;
+	case T_LEFTBRACET:
+		ret_value = ERR_RIGHT;
+		break;
+	default:
+		break;
 	}
 
 	return ret_value;
@@ -458,43 +484,66 @@ int p_idlist() {
 }
 
 int p_idnext() {
-	
+
 	int ret_value = ERR_SYNAN;
 
 	switch (token.type) {
-		case T_COMMA:
+	case T_COMMA:
+		GET_TOKEN();
+
+		if (token.type == T_ID) {
 			GET_TOKEN();
 
-			if (token.type == T_ID) {
-				GET_TOKEN();
+			ret_value = p_idnext();
+			VALUE_CHECK();
+		}
 
-				ret_value = p_idnext();
-				VALUE_CHECK();
-			}
+		break;
 
-			break;
+	case T_ASSIGN:
+		GET_TOKEN();
 
-		case T_ASSIGN:
-			GET_TOKEN();
-
-			ret_value = ERR_RIGHT;
-			break;
-		default:
-			break;
+		ret_value = ERR_RIGHT;
+		break;
+	default:
+		break;
 	}
 
 	return ret_value;
 }
 
 int p_expressionlist() {
-	//#TODO ZATIAL KEBABOVA IMPLEMENTACIA
-	int ret_value=ERR_SYNAN;
+	
+	int ret_value = ERR_SYNAN;
+
+	ret_value = expression();
+	VALUE_CHECK();
+	ret_value = p_expressionnext();
+	VALUE_CHECK();
 
 	return ret_value;
 }
 
 int p_expressionnext() {
-	int ret_value=ERR_SYNAN;
+
+	int ret_value = ERR_SYNAN;
+
+	switch (token.type) {
+		case T_COMMA:
+			GET_TOKEN();
+
+			ret_value = expression();
+			VALUE_CHECK();
+			ret_value = p_expressionnext();
+			VALUE_CHECK();
+			break;
+		case T_LEFTBRACET:
+		case T_EOL:
+			ret_value = ERR_RIGHT;
+			break;
+		default:
+			break;
+	}
 	return ret_value;
 }
 
@@ -503,23 +552,23 @@ int p_termlist() {
 	int ret_value = ERR_SYNAN;
 
 	switch (token.type) {
-		case T_ID:
-		case T_INT:
-		case T_DOUBLE:
-		case T_STRING:
-			ret_value = p_term();
-			VALUE_CHECK();
-			ret_value = p_termnext();
-			VALUE_CHECK();
-			break;
+	case T_ID:
+	case T_INT:
+	case T_DOUBLE:
+	case T_STRING:
+		ret_value = p_term();
+		VALUE_CHECK();
+		ret_value = p_termnext();
+		VALUE_CHECK();
+		break;
 
-		case T_RIGHTBR:
-			GET_TOKEN();
+	case T_RIGHTBR:
+		GET_TOKEN();
 
-			ret_value = ERR_RIGHT;
-			break;
-		default:
-			break;
+		ret_value = ERR_RIGHT;
+		break;
+	default:
+		break;
 	}
 
 	return ret_value;
@@ -530,22 +579,22 @@ int p_termnext() {
 	int ret_value = ERR_SYNAN;
 
 	switch (token.type) {
-		case T_COMMA:
-			GET_TOKEN();
+	case T_COMMA:
+		GET_TOKEN();
 
-			ret_value = p_term();
-			VALUE_CHECK();
-			ret_value = p_termnext();
-			VALUE_CHECK();
-			break;
+		ret_value = p_term();
+		VALUE_CHECK();
+		ret_value = p_termnext();
+		VALUE_CHECK();
+		break;
 
-		case T_RIGHTBR:
-			GET_TOKEN();
+	case T_RIGHTBR:
+		GET_TOKEN();
 
-			ret_value = ERR_RIGHT;
-			break;
-		default:
-			break;
+		ret_value = ERR_RIGHT;
+		break;
+	default:
+		break;
 	}
 
 	return ret_value;
@@ -579,48 +628,101 @@ int p_term() {
 		ret_value = ERR_RIGHT;
 		break;
 	default:
-			break;
+		break;
 	}
 
 	return ret_value;
 }
 
 int p_idstat() {
-	// #TODO
-	int ret_value=ERR_SYNAN;
+
+	int ret_value = ERR_SYNAN;
+	
+	switch (token.type) {
+	case T_DOUBLEDOT:
+		GET_TOKEN();
+		
+		ret_value = expression();
+		VALUE_CHECK();
+		break;
+
+	case T_COMMA:
+	case T_ASSIGN:
+	
+		ret_value = p_idnext();
+		VALUE_CHECK();
+		ret_value = p_exprorid();
+		VALUE_CHECK();
+		break;
+	case T_LEFTBR:
+		GET_TOKEN();
+
+		ret_value = p_termlist();
+		VALUE_CHECK();
+		break;
+	default:
+		break;
+	}
 	return ret_value;
 }
 
 int p_exprorid() {
-	// #TODO
-	int ret_value=ERR_SYNAN;
+	
+	int ret_value = ERR_SYNAN;
+	
+	switch (token.type) {
+		case T_INT:
+		case T_DOUBLE:
+		case T_STRING:
+		case T_LEFTBR:
+			ret_value = p_expressionlist();
+			VALUE_CHECK();
+			break;
+		case T_ID:
+			PEEK_TOKEN();
+			
+			if (tokenp.type == T_LEFTBR) {
+				GET_TOKEN();
+				GET_TOKEN();
+
+				ret_value = p_termlist();
+				VALUE_CHECK();
+			}
+			else {
+				ret_value = p_expressionlist();
+				VALUE_CHECK();
+			}
+			break;
+		default:
+			break;
+	}
 	return ret_value;
 }
 
 int p_opteol() {
-	
+
 	int ret_value = ERR_SYNAN;
 
 	switch (token.type) {
-		case T_EOL:
-			GET_TOKEN();
+	case T_EOL:
+		GET_TOKEN();
 
-			ret_value = p_opteol();
-			VALUE_CHECK();
-			break;
+		ret_value = p_opteol();
+		VALUE_CHECK();
+		break;
 
-		case T_WPACKAGE:
-		case T_END_OF_FILE:
-		case T_WFUNC:
-		case T_WIF:
-		case T_WFOR:
-		case T_ID:
-		case T_WRETURN:
-		case T_RIGHTBRACET:
-			ret_value = ERR_RIGHT;
-			break;
-		default:
-			break;
+	case T_WPACKAGE:
+	case T_END_OF_FILE:
+	case T_WFUNC:
+	case T_WIF:
+	case T_WFOR:
+	case T_ID:
+	case T_WRETURN:
+	case T_RIGHTBRACET:
+		ret_value = ERR_RIGHT;
+		break;
+	default:
+		break;
 	}
 
 	return ret_value;
