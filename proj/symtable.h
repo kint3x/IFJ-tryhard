04fff50fabe782d7ec50c8002fe85a09a200d801
Kebab <martin@matejka.xyz>
2020-11-15 11:39:37 +0100

    hlavicka

---
+++ proj/symtable.h
@@ -0,0 +1,20 @@
+/**
+ * Projekt: Implementácia tabulky symbolov pre jazyk IFJ20
+ *
+ * @brief Implementácia tabulky symbolov.
+ *
+ * @author Martin Matějka <xmatej55@vutbr.cz>
+ */
+
+#ifndef _SYMTABLE_H
+#define _SYMTABLE_H
+
+#include <stdlib.h>
+
+typedef struct sym_item{
+
+}*sym_itemPtr;
+
+
+
+#endif
