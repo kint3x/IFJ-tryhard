fa11156e072dca3728fc2eb7da79aa6908c507ff
Kebab <martin@matejka.xyz>
2020-11-26 02:39:09 +0100

    pridanie generovanie funkcie

--- proj/code_generator.c
+++ proj/code_generator.c
@@ -10,14 +10,63 @@
 //premenne pre generator zacinaju &
 //uzivatelske zacinaju s %
 
-#define GEN_HEADER\
+#define GEN_HEADER \
 "\n#Start of program"\
-"\n .ifjcode20"\
-"\n JUMP $main"
-
+"\n.ifjcode20"\
+"\nCREATEFRAME"\
+"\nCALL $main" \
+"\nJUMP end"
 
 #define FUNCTION_INPUTI \
-"\n #FUNCTION INPUTI" \
+"\n#FUNCTION INPUTI" \
 
 
 #define GEN_END "\nLABEL END"
+
+Nstring generated_code;
+
+#define ADD_CODE(_code) if(!nstring_add_str(&generated_code,(_code))) return false;
+
+
+bool generate_start(){
+	ADD_CODE(GEN_HEADER);
+
+	return true;
+}
+
+bool G_Fun_header(Nstring *name){
+	ADD_CODE("\n#FUNCTION "); ADD_CODE("$");ADD_CODE(name->string);
+	ADD_CODE("\nPUSHFRAME");
+	ADD_CODE("\nDEFVAR LF@&bin")
+	return true;
+}
+bool G_Fun_argument(Nstring *s,int poradie){
+	char buf[30];
+	sprintf(buf,"%d",poradie);
+	ADD_CODE("\nDEFVAR ");ADD_CODE("%");ADD_CODE(s->string);ADD_CODE(buf);
+	ADD_CODE("\nMOVE LF@&arg");ADD_CODE(buf);
+	return true;
+}
+bool G_Fun_def_return(int poradie){
+	char buf[30];
+	sprintf(buf,"%d",poradie);
+	ADD_CODE("\nDEFVAR LF@&ret");ADD_CODE(buf);
+	return true;
+}
+
+bool G_Fun_ret_value(int poradie){
+	char buf[30];
+	sprintf(buf,"%d",poradie);
+	ADD_CODE("\nDEFVAR LF@&ret")ADD_CODE(buf);
+	return true;
+}
+
+bool G_Fun_return(){
+	ADD_CODE("\nPOPFRAME");
+	ADD_CODE("\nRETURN");
+	return true;
+}
+void G_PRINT(){
+	printf("%s\n",generated_code.string);
+	free(generated_code.string);
+}
--- proj/code_generator.h
+++ proj/code_generator.h
@@ -7,10 +7,13 @@
 
 #include "dynstring.h"
 #include <string.h>
+#include "symtable.h"
 
-
-
-
-
-
+bool generate_start();
+bool G_Fun_header(Nstring *name);
+bool G_Fun_argument(Nstring *s,int poradie);
+bool G_Fun_def_return(int poradie);
+bool G_Fun_ret_value(int poradie);
+bool G_Fun_return();
+void G_PRINT();
 #endif
